cdd080ce9b83af21c3a470900f43840c377b1064
Andrew Venson <vensoneandrew@gmail.com>
2020-04-21 12:46:03 -0500

    added react router

--- drews_portfolio/src/App.js
+++ drews_portfolio/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import NavComponent from "./components/NavComponent";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavComponent />
-    </div>
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </Router>
   );
 }
 
