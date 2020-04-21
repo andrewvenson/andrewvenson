635453c27ed9bb53285cfaab8394bd9daec9bc63
Andrew Venson <vensoneandrew@gmail.com>
2020-04-21 12:31:56 -0500

    installed react router dependencies

--- drews_portfolio/src/App.js
+++ drews_portfolio/src/App.js
@@ -2,15 +2,6 @@ import React from "react";
 import "./App.css";
 import NavComponent from "./components/NavComponent";
 
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
-
 function App() {
   return (
     <div className="App">
