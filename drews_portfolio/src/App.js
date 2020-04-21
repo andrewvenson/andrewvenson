9a5224ab447c192882629e40c9cf02a0e1bfc49a
Andrew Venson <vensoneandrew@gmail.com>
2020-04-21 12:13:37 -0500

    installed bootstrap dependencies, added nav component

--- drews_portfolio/src/App.js
+++ drews_portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavComponent from "./components/NavComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <NavComponent />
     </div>
   );
 }
---
+++ drews_portfolio/src/components/NavComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavComponent() {
+  return (
+    <header>
+      <nav class="myNav">
+        <h1>Hello World</h1>
+      </nav>
+    </header>
+  );
+}
+
+export default NavComponent;
