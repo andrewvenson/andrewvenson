344637e332bb35486446d874f6900d84199f32bd
Andrew Venson <vensoneandrew@gmail.com>
2020-04-21 12:20:19 -0500

    added react bootstrap navbar

--- drews_portfolio/src/App.js
+++ drews_portfolio/src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./App.css";
 import NavComponent from "./components/NavComponent";
 
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
 function App() {
   return (
     <div className="App">
