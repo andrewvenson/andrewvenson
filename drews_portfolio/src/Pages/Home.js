02a214fae8ef6070553354b636e89f598a342d71
Andrew Venson <vensoneandrew@gmail.com>
2020-04-22 00:15:02 -0500

    direction div

--- drews_portfolio/src/Pages/Home.js
+++ drews_portfolio/src/Pages/Home.js
@@ -20,12 +20,25 @@ const Home = () => {
     backgroundSize: "cover",
   };
 
+  const infoBox = {
+    width: "300px",
+    borderRadius: "10px",
+    border: "1px solid lightgray",
+    padding: "10px",
+    marginTop: "15px",
+  };
+
   const anywhere = () => {
     window.location.href = "/about";
   };
 
   const onHover = {
     boxShadow: "3px 5px 8px black",
+  };
+
+  const directions = {
+    fontWeight: "bolder",
+    color: "purple",
   };
   return (
     <div>
@@ -36,6 +49,16 @@ const Home = () => {
             className="portfolioPic"
             style={portfolioPic}
           ></div>
+        </div>
+        <div style={infoBox}>
+          <p>
+            Yup, that big goofy head in the center of the page is me.
+            <span style={directions}> Drag that head to the LEFT </span> to view
+            my projects. <span style={directions}>Drag me to the RIGHT </span>
+            to get a little information about me. If you would like to contact
+            me just
+            <span style={directions}> drag me DOWN</span>.
+          </p>
         </div>
       </Container>
     </div>
