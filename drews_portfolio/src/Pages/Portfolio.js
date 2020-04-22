dc22a29a269d64a4ddaec4f6721a0ff80d76b499
Andrew Venson <vensoneandrew@gmail.com>
2020-04-22 18:21:49 -0500

    added project component

--- drews_portfolio/src/Pages/Portfolio.js
+++ drews_portfolio/src/Pages/Portfolio.js
@@ -2,13 +2,26 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Head from "../components/Head";
 import Title from "../components/Title";
+import PortfolioProjects from "../components/PortfolioProjects";
 
 const Portfolio = () => {
+  let ad = "these are the detials of the app";
+  const projectContainer = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "50vh",
+  };
   return (
     <div>
       <Container>
         <Head />
         <Title name="Portfolio" />
+        <div style={projectContainer}>
+          <PortfolioProjects appName="Pycdv" appDetails={ad} />
+          <PortfolioProjects appName="Pycdv" appDetails={ad} />
+          <PortfolioProjects appName="Pycdv" appDetails={ad} />
+        </div>
       </Container>
     </div>
   );
---
+++ drews_portfolio/src/components/PortfolioProjects.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PortfolioProjects = (props) => {
+  const Project = {
+    border: "1px solid lightgray",
+    width: "300px",
+    height: "275px",
+    margin: "10px",
+    padding: "10px",
+    borderRadius: "10px",
+    boxShadow: "3px 4px 15px lightgray",
+  };
+  return (
+    <div style={Project}>
+      <h4>{props.appName}</h4>
+      <p>{props.appDetails}</p>
+      <a>Like this site? Contact me for more information</a>
+    </div>
+  );
+};
+
+export default PortfolioProjects;
