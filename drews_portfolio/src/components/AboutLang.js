d47b66dbfe7e6707d0b074ab3dacff647a888438
Andrew Venson <vensoneandrew@gmail.com>
2020-04-22 23:28:00 -0500

    added sql, about page almost done

--- drews_portfolio/src/components/AboutLang.js
+++ drews_portfolio/src/components/AboutLang.js
@@ -3,32 +3,38 @@ import ReactLogo from "../images/react.png";
 import PythonLogo from "../images/python.png";
 import PhpLogo from "../images/php.png";
 import JsLogo from "../images/javascript.png";
+import MsLogo from "../images/mysql.png";
 
 const AboutLang = () => {
   return (
-    <div>
-      <section>
-        <p style={{ color: "gray", display: "flex", justifyContent: "center" }}>
-          My Languages
-        </p>
-        <img
-          src={ReactLogo}
-          style={{ width: "60px", height: "60px", margin: "10px" }}
-        />
-        <img
-          src={PythonLogo}
-          style={{ width: "60px", height: "60px", margin: "10px" }}
-        />
-        <img
-          src={PhpLogo}
-          style={{ width: "60px", height: "60px", margin: "10px" }}
-        />
-        <img
-          src={JsLogo}
-          style={{ width: "60px", height: "60px", margin: "10px" }}
-        />
-      </section>
-    </div>
+    <section>
+      <p
+        className="myLangy"
+        style={{ color: "gray", display: "flex", justifyContent: "center" }}
+      >
+        My Languages
+      </p>
+      <img
+        src={ReactLogo}
+        style={{ width: "60px", height: "60px", margin: "10px" }}
+      />
+      <img
+        src={PythonLogo}
+        style={{ width: "60px", height: "60px", margin: "10px" }}
+      />
+      <img
+        src={PhpLogo}
+        style={{ width: "60px", height: "60px", margin: "10px" }}
+      />
+      <img
+        src={JsLogo}
+        style={{ width: "60px", height: "60px", margin: "10px" }}
+      />
+      <img
+        src={MsLogo}
+        style={{ width: "60px", height: "60px", margin: "10px" }}
+      />
+    </section>
   );
 };
 
