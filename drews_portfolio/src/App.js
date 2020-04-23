4f513f0ae1c9838b0175376a8234bacb1d7e9dc4
Andrew Venson <vensoneandrew@gmail.com>
2020-04-23 16:58:42 -0500

    routing'

--- drews_portfolio/src/App.js
+++ drews_portfolio/src/App.js
@@ -6,20 +6,26 @@ import Home from "./Pages/Home";
 import Portfolio from "./Pages/Portfolio";
 import Contact from "./Pages/Contact";
 import HomeRun from "./components/HomeRun";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename="/">
       <NavComponent />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/drews_portfolio" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Portfolio} />
       </Switch>
       <HomeRun />
-    </Router>
+    </HashRouter>
   );
 }
 
