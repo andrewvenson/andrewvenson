58aaf433b39e8dfb319c92a51f5a77be443eb699
Andrew Venson <vensoneandrew@gmail.com>
2020-04-25 20:35:32 -0500

    added bootstrap modal on resume click

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
 import resume from "../images/resume.PNG";
 
 const Resume = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Link to="/">
-      <img
-        className="resume"
-        src={resume}
-        style={{
-          width: "50px",
-          height: "50px",
-          filter: "drop-shadow(12px 12px 25px rgba(0,0,0,0.5))",
-          borderRadius: "5px",
-        }}
-        alt="Resume"
-      />
-    </Link>
+    <>
+      <Link onClick={handleShow}>
+        <img
+          className="resume"
+          src={resume}
+          style={{
+            width: "50px",
+            height: "50px",
+            filter: "drop-shadow(12px 12px 25px rgba(0,0,0,0.5))",
+            borderRadius: "5px",
+          }}
+          alt="Resume"
+        />
+      </Link>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
