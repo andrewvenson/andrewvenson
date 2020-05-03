1997bed399ee7692bc527c6f2c22380faee48340
Andrew Venson <vensoneandrew@gmail.com>
2020-05-03 13:53:02 -0500

    added project to portfolio

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -6,12 +6,16 @@ import Title from "../components/Title";
 import PortfolioProjects from "../components/PortfolioProjects";
 import PyCdv from "../images/pycdv.PNG";
 import Tbc from "../images/tbc.jpg";
+import SecurePw from "../images/pwgenerated.PNG";
 
 const Portfolio = () => {
   // projecttext
-  let ad =
+  let pycdv =
     "PyCDV is a website that compiles statistics, facts, and information about our current COVID-19 pandemic";
   let lorem = "Pending creation...";
+
+  let securepwgenerator =
+    "An intuitive password generator application that creates a random secure string of characters based on your required criteria.";
   const projectContainer = {
     marginBottom: "65px",
   };
@@ -30,15 +34,16 @@ const Portfolio = () => {
             <PortfolioProjects
               appUrl="https://pycdv.com"
               appName="Pycdv"
-              appDetails={ad}
+              appDetails={pycdv}
               appImage={PyCdv}
             />
           </Col>
           <Col style={colStyling}>
             <PortfolioProjects
-              appImage={Tbc}
-              appName="To Be Continued"
-              appDetails={lorem}
+              appUrl="https://securepwgenerator.herokuapp.com"
+              appImage={SecurePw}
+              appName="SecurePWGenerator"
+              appDetails={securepwgenerator}
             />
           </Col>
           <Col style={colStyling}>
