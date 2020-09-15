9882f864c1efe55201a07598d026af79c58548ad
Andrew E. Venson <Andrew.Venson@integralcare.org>
2020-09-14 19:34:50 -0500

    integrations & automations

--- src/components/AboutLang.js
+++ src/components/AboutLang.js
@@ -5,42 +5,76 @@ import PythonLogo from "../images/python.png";
 import PhpLogo from "../images/php.png";
 import JsLogo from "../images/javascript.png";
 import MsLogo from "../images/mysql.png";
+import Outlook from "../images/outlook.png";
+import Netsmart from "../images/netsmart.png";
+import Teams from "../images/teams.png";
+import Genesys from "../images/genesys.PNG";
 
 const AboutLang = () => {
   return (
-    <section className="langs">
-      <p
-        className="myLangy"
-        style={{ color: "gray", display: "flex", justifyContent: "center" }}
-      >
-        My Languages
-      </p>
-      <img
-        src={ReactLogo}
-        style={{ width: "60px", height: "60px", margin: "10px" }}
-        alt="React"
-      />
-      <img
-        src={PythonLogo}
-        style={{ width: "60px", height: "60px", margin: "10px" }}
-        alt="Python"
-      />
-      <img
-        src={PhpLogo}
-        style={{ width: "60px", height: "60px", margin: "10px" }}
-        alt="Php"
-      />
-      <img
-        src={JsLogo}
-        style={{ width: "60px", height: "60px", margin: "10px" }}
-        alt="Javascript"
-      />
-      <img
-        src={MsLogo}
-        style={{ width: "60px", height: "60px", margin: "10px" }}
-        alt="MySql"
-      />
-    </section>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <section className="langs">
+        <p
+          className="myLangy"
+          style={{ color: "gray", display: "flex", justifyContent: "center" }}
+        >
+          My Languages
+        </p>
+        <img
+          src={ReactLogo}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="React"
+        />
+        <img
+          src={PythonLogo}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Python"
+        />
+        <img
+          src={PhpLogo}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Php"
+        />
+        <img
+          src={JsLogo}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Javascript"
+        />
+        <img
+          src={MsLogo}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="MySql"
+        />
+      </section>
+      <section>
+        <p
+          className="myLangy"
+          style={{ color: "gray", display: "flex", justifyContent: "center" }}
+        >
+          Integrations & Automations
+        </p>
+        <img
+          src={Outlook}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Outlook"
+        />
+        <img
+          src={Teams}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Teams"
+        />
+        <img
+          src={Netsmart}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Netsmart"
+        />
+        <img
+          src={Genesys}
+          style={{ width: "60px", height: "60px", margin: "10px" }}
+          alt="Genesys"
+        />
+      </section>
+    </div>
   );
 };
 
