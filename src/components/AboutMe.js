6141b630fb5d1cecd68a69fbce20bb84af4116eb
Andrew E. Venson <Andrew.Venson@integralcare.org>
2020-09-14 19:12:58 -0500

    added new bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,13 +16,9 @@ const AboutMe = () => {
     <article>
       <p className="AboutMeText" style={AboutMeText}>
         My name is Andrew, a self taught developer that just cant shy away from
-        a good learning opportunity. I fell in love with the art of coding from
-        being exposed to the c++ programming language at a very young age. My
-        enchantment with programming started with a small desire to be smarter
-        than myself. I have a love for learning and enrichment, and I've found
-        that I am very good with soaking up a concept and making it fit my own
-        style. My primary focus right now is web development, and those are the
-        languages to the right that I can provide help in.
+        a good learning opportunity. I'm experienced in automations,
+        integrations, web development, and scripting. Those to the right, are
+        some of the languages I'm experienced in.
       </p>
     </article>
   );
